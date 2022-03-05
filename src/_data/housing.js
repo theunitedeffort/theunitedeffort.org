cf133092903fa6706826b33ec763c451125801ad
Phil Hawksworth <phil@netlify.com>
2022-03-05 15:43:14 +0000

    replace deleted fetch

--- src/_data/housing.js
+++ src/_data/housing.js
@@ -1,12 +1,18 @@
 var Airtable = require('airtable');
 var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('app7w5VUuXtmU2nJq');
 
+
+
 let housingList = [];
 
 async function fetchHousingList() {
+
   await base('Housing list').select({
+    // Selecting the first 3 records in All housing listing:
+    // maxRecords: 3,
     view: "All housing listing"
   }).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
 
     records.forEach(function(record) {
       housingList.push({
@@ -15,9 +21,15 @@ async function fetchHousingList() {
       })
     });
 
+    fetchNextPage();
+
   });
+
   return housingList;
+
 }
+
+
 
 module.exports = async function() {
   await fetchHousingList();
