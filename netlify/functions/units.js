e49bf055f01b9d19be3cd5145ec25366fd11665e
trevorshannon <trevor.j.shannon@gmail.com>
2022-05-02 12:06:53 -0700

    [WIP] fixes metadata

--- netlify/functions/units.js
+++ netlify/functions/units.js
@@ -8,17 +8,18 @@ const NO_MAX_INCOME_STRING = NO_MIN_INCOME_STRING;
 
 
 // Make a definition list from all the data returned about this item
-const metaData = (data) => {
-  if (data[0]) {
-    let meta = data[0].record;
-    return `<dl>    
-    <dt>Address</dt><dd>${meta["Address (from Housing)"]?.[0]|| " "}</dd>
-    <dt>City</dt><dd>${meta["City (from Housing)"]?.[0]|| " "}</dd>
-    <dt>Phone</dt><dd>${meta["Phone (from Housing)"]?.[0] || " "} </dd>
-    <dt>Number of units</dt><dd>${meta["UNITS_CNT (from Housing)"]?.[0] || " "}</dd>
-    <dt>Website</dt><dd><a href="${meta["URL (from Housing)"]?.[0] || " "}" target="_BLANK" rel="noopener">${meta["URL (from Housing)"]?.[0] || " "}</a></dd>
-    </dl>`;
+const metaData = (units) => {
+  let aptName = units.metadata.aptName || "Apartment Listing";
+  let heading = `<h1>${aptName}</h1>`
+  let definitions = [];
+  for (key in units.metadata) {
+    if (key === 'aptName') { continue; }
+    definitions.push(`<dt>${key}</dt><dd>${units.metadata[key]}</dd>`);
+    // TODO (trevorshannon): add link a tag.
   }
+  return `
+    ${heading}
+    <dl>${definitions.join("")}</dl>`;
 }
 
 const formatCurrency = (value) => {
@@ -58,11 +59,9 @@ const unitDetails = (data) => {
 }
 
 
-const unitTables = (data) => {
-  //let aptName = data[0].record["APT_NAME"]?.[0] || " ";
-  let aptName = "The Apartment Name";  // TODO(trevorshannon)
+const unitTables = (units) => {
   let tables = [];
-  let unitTypes = Object.keys(data).sort();
+  let unitTypes = Object.keys(units.data).sort();
   for (idx in unitTypes) {
     tables.push(`
       <h3>${unitTypes[idx]}</h3>
@@ -77,14 +76,13 @@ const unitTables = (data) => {
           </tr>
         </thead>
         <tbody>
-        ${unitDetails(data[unitTypes[idx]])}
+        ${unitDetails(units.data[unitTypes[idx]])}
         </tbody>
       </table>
     `);
   }
   return `
-      <h1>${aptName}</h1>
-      ${metaData(data)}
+      ${metaData(units)}
       ${tables.join("")}
   `;
 };
@@ -93,24 +91,35 @@ const unitTables = (data) => {
 // Lookup data for this item from the Airtable API
 const fetchData = async(housingID) => {
   const table = base("tblNLrf8RTiZdY5KN"); // Units table
-  let newunits = {};
   return table.select({
       view: "API all units",
       filterByFormula: `HOUSING_LIST_ID = "${housingID}"`
     })
     .all()
     .then(records => {
-      let units = [];
-      for (record in records) {
-        units.push({
-          record: (records[record].fields)
-        })
-        let unitKey = records[record].fields.TYPE;
-        newunits[unitKey] = newunits[unitKey] || [];
-        newunits[unitKey].push({record: records[record].fields});
+      let units = {"metadata": {}, "data": []};
+      if (records[0]){
+        console.log(records[0]);
+        units.metadata["aptName"] = (
+          records[0].fields["APT_NAME"]?.[0]|| " ");
+        units.metadata["Address"] = (
+          records[0].fields["Address (from Housing)"]?.[0]|| " ");
+        units.metadata["City"] = (
+          records[0].fields["City (from Housing)"]?.[0]|| " ");
+        units.metadata["Phone"] = (
+          records[0].fields["Phone (from Housing)"]?.[0]|| " ");
+        units.metadata["Number of Units"] = (
+          records[0].fields["UNITS_CNT (from Housing)"]?.[0]|| " ");
+        units.metadata["Website"] = (
+          records[0].fields["URL (from Housing)"]?.[0]|| " ");
       }
-      console.log(newunits);
-      return newunits;
+      for (record in records) {
+        let unitKey = records[record].fields.TYPE;
+        units.data[unitKey] = units.data[unitKey] || [];
+        units.data[unitKey].push({record: records[record].fields});
+      }
+      console.log(units);
+      return units;
     });
 };
 
