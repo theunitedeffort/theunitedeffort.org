6efe20a0c8cad24b39898def54e5568c7380c27d
trevorshannon <trevor.j.shannon@gmail.com>
2022-05-03 14:34:54 -0700

    Adds custom sorting function for unit type
    Note some code duplication until the property page is migrated over to use liquid templates and units.js is moved into .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -58,6 +58,33 @@ module.exports = function(eleventyConfig) {
     return filtered;
   });
 
+  eleventyConfig.addFilter("sortUnitType", function(values, property='') {
+    let sorted = values.sort(function(a, b) {
+      let valA = property ? a[property] : a;
+      let valB = property ? b[property] : b;
+      // Return < 0 if 'a' should go before 'b'
+      // Return > 0 if 'b' should go before 'a'
+      if ((valA === "SRO" && valB === "Studio") || // SRO before Studio.
+          (valA === "SRO" || valA === "Studio") || // SRO/Studio always first.
+          (valB === "Others")) { // Others always last.
+        return -1; 
+      }
+      if ((valA === "Studio" && valB === "SRO") || // SRO before Studio
+          (valB === "SRO" || valB === "Studio") || // SRO/Studio always first.
+          (valA === "Others")) { // Others always last.
+        return 1;
+      }
+      if (valA < valB) {
+        return -1;
+      }
+      if (valA > valB) {
+        return 1;
+      }
+      return 0;
+    });
+    return sorted;
+  });
+
   // Add filter checkbox state from the query parameters to 'filterValues'. 
   eleventyConfig.addFilter("updateFilterState", function(filterValues, query) {
     // If there is no query (such as on the affordable housing landing page)
