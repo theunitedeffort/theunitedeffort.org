d3e3311eac5c20b92761b01337dd8b32a0317d8c
trevorshannon <trevor.j.shannon@gmail.com>
2022-05-25 14:45:18 -0700

    Tweaks column widths and fixes filterOptions bug

--- src/site/serverless/affordable-housing.11tydata.js
+++ src/site/serverless/affordable-housing.11tydata.js
@@ -23,7 +23,7 @@ function FilterCheckbox(name, selected = false) {
 // TODO(trevorshannon): Only fetch filter options at build time, not on every
 // page load.
 const fetchFilterOptions = async() => {
-  let records = [];
+  let options = [];
   const table = base(UNITS_TABLE);
 
   return table.select({
@@ -37,14 +37,14 @@ const fetchFilterOptions = async() => {
         if (record.get("City (from Housing)") !== undefined) {
           cityStr = record.get("City (from Housing)")[0];
         }
-        records.push({
+        options.push({
           city: cityStr,
           openStatus: record.get("STATUS"),
           unitType: record.get("TYPE"),
           populationsServed: record.get("_POPULATIONS_SERVED"),
         })
       });
-      return records;
+      return options;
     });
 };
 
@@ -67,25 +67,24 @@ module.exports = async function() {
   }
   console.log("Fetching filter options.");
   let filterOptions = await fetchFilterOptions();
-  let cities = [...new Set(filterOptions.map(({ city }) => city))];
-  cities = cities.filter(city => city !== undefined);
-  let openStatuses = [...new Set(filterOptions.map(({ openStatus }) => openStatus))];
-  openStatuses = openStatuses.filter(openStatus => openStatus !== undefined);
-  let unitTypes = [...new Set(filterOptions.map(({ unitType }) => unitType))];
-  unitTypes = unitTypes.filter(unitType => unitType !== undefined);
-  let allPopulationsServed = filterOptions.map(({ populationsServed }) => populationsServed);
+  let cities = [...new Set(filterOptions.map(o => o.city))];
+  cities = cities.filter(x => x !== undefined);
+  let openStatuses = [...new Set(filterOptions.map(o => o.openStatus))];
+  openStatuses = openStatuses.filter(x => x !== undefined);
+  let unitTypes = [...new Set(filterOptions.map(o => o.unitType))];
+  unitTypes = unitTypes.filter(x => x !== undefined);
+  let allPopulationsServed = filterOptions.map(o => o.populationsServed);
   allPopulationsServed = [...new Set(allPopulationsServed.flat())];
-  allPopulationsServed = allPopulationsServed.filter(populationsServed => populationsServed !== undefined);
-
+  allPopulationsServed = allPopulationsServed.filter(x => x !== undefined);
 
   let filterVals = [
-    new FilterSection("City", "city", cities.map((x) => new FilterCheckbox(x))),
+    new FilterSection("City", "city", cities.map(x => new FilterCheckbox(x))),
     new FilterSection("Type of Unit", "unitType",
-      unitTypes.map((x) => new FilterCheckbox(x))),
+      unitTypes.map(x => new FilterCheckbox(x))),
     new FilterSection("Availability", "availability",
-      openStatuses.map((x) => new FilterCheckbox(x))),
+      openStatuses.map(x => new FilterCheckbox(x))),
     new FilterSection("Populations Served", "populationsServed",
-      allPopulationsServed.map((x) => new FilterCheckbox(x))),
+      allPopulationsServed.map(x => new FilterCheckbox(x))),
     
   ];
   console.log("Got filter options.");
