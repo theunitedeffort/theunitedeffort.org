5075dddee4adc03d26ffc4f09320da0a983270f0
trevorshannon <trevor.j.shannon@gmail.com>
2022-06-03 14:04:02 -0700

    WIP form submission testing

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -4,6 +4,7 @@ var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.e
 const AFFORDABLE_HOUSING_CHANGES_TABLE = "tblXy0hiHoda5UVSR";
 
 exports.handler = async function(event) {
+  console.log(event.body.payload);
   let table = base(AFFORDABLE_HOUSING_CHANGES_TABLE);
   let formResponses = event.body.payload.data;
   console.log(JSON.parse(formResponses));
