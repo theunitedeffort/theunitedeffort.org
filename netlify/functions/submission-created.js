1b0ecfe73db342aab7878ca74599fc2d45aefbb1
trevorshannon <trevor.j.shannon@gmail.com>
2022-06-03 13:58:08 -0700

    WIP test airtable record creation on form submission

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,4 +1,19 @@
+var Airtable = require('airtable');
+var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_ID);
+
+const AFFORDABLE_HOUSING_CHANGES_TABLE = "tblXy0hiHoda5UVSR";
+
 exports.handler = async function(event) {
-  console.log(event);
-  console.log(JSON.stringify(event, null, 2));
+  let table = base(AFFORDABLE_HOUSING_CHANGES_TABLE);
+  let formResponses = event.body.payload.data;
+  console.log(JSON.parse(formResponses));
+  // TODO: error handling. 
+  table.create({
+    "CAMPAIGN": "First Campaign",
+    "FORM_RESPONSE_JSON": formResponses,
+  }, function(err, record) {
+    if (err) {
+      console.error(err);
+    }
+  });
 };
