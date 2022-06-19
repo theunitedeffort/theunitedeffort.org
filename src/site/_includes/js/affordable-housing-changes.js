79ded4d268a32fab4fb6178fc62a55c73e428eba
trevorshannon <trevor.j.shannon@gmail.com>
2022-06-19 10:13:08 -0700

    Adds user name input and storage.

--- src/site/_includes/js/affordable-housing-changes.js
+++ src/site/_includes/js/affordable-housing-changes.js
@@ -1,3 +1,5 @@
+const USER_NAME_KEY = "userName";
+
 // Submits the housing changes form.
 function submitForm() {
   // Rather than use a <button type="submit">, handle the submission
@@ -39,11 +41,41 @@ function setEmptyQueueMessage(queue) {
 // Displays an error message if the housing ID requested does not exist.
 function setInvalidHousingIdMessage(housingId, campaignPath) {
 	let header = "Oops! That property doesn't seem to exist.";
-	let content = `There is no property with the ID \
-	  <span class="bold">${housingId}</span> in our database.  Check your URL \
-	  and try again or see the <a href="${campaignPath}">next property</a> that \
+	let content = `There is no property with the ID
+	  <span class="bold">${housingId}</span> in our database.  Check your URL
+	  and try again or see the <a href="${campaignPath}">next property</a> that
 	  needs to be checked.`;
 	setTerminalMessage(header, content);
+}
+
+function setUserName() {
+  let userNameInput = document.getElementById("user-name-input");
+  let userName = userNameInput.value.trim();
+  if (userName) {
+	  document.getElementById("user-name-input-container").setAttribute("hidden",
+	  	"hidden");
+	  document.getElementById("edit-user-name").removeAttribute("hidden");
+		document.getElementById("user-name").setAttribute("value", userName);
+		userNameInput.setAttribute("value", userName);
+		document.getElementById("welcome-name").textContent = `, ${userName}`;
+		localStorage.setItem(USER_NAME_KEY, userName);
+	}
+}
+
+function editUserName() {
+  document.getElementById("user-name-input-container").removeAttribute(
+  	"hidden");
+  document.getElementById("edit-user-name").setAttribute("hidden", "hidden");
+	document.getElementById("welcome-name").textContent = "";
+	document.getElementById("user-name-input").focus();
+}
+
+function handleUserNameKeydown(e) {
+  // Allow the user to re-commit their name even if they don't make any changes to it.
+  if (e.code === "Enter") {
+    let inputUserName = document.getElementById("user-name-input");
+    inputUserName.dispatchEvent(new Event("change"));
+  }
 }
 
 // Gets the campaign and optional housing ID from the URL.
@@ -66,6 +98,9 @@ function getPathParams() {
 
 // Fetches all data required to prefill the input form fields.
 async function fetchFormPrefillData(params) {
+	if (!params.campaign) {
+		return {};
+	}
 	let fetchPath = encodeURI(
 		`/contrib/affordable-housing/next-property/${params.campaign}`);
 	if (params.housingId) {
@@ -79,26 +114,52 @@ async function fetchFormPrefillData(params) {
 }
 
 function addListeners() {
+	// Form submission
 	let submitButtons = document.querySelectorAll("[id^=submit-button]");
   for (button of submitButtons) {
     button.addEventListener("click", submitForm);
   }
+
+  // User name
+  document.getElementById("edit-user-name").addEventListener("click",
+  	editUserName);
+  let userNameInput = document.getElementById("user-name-input");
+  userNameInput.addEventListener("change", setUserName);
+  userNameInput.addEventListener("focusout", setUserName);
+  userNameInput.addEventListener("keydown", handleUserNameKeydown);
+}
+
+// 
+function initUserName() {
+  let userName = localStorage.getItem(USER_NAME_KEY);
+	let userNameInput = document.getElementById("user-name-input");
+	if (userName) {
+		userNameInput.setAttribute("value", userName);
+		userNameInput.dispatchEvent(new Event("change")); 
+	}
 }
 
 // Prepares the page for display to the user.
 function initPage(data, params) {
-  if(data.housing) {
-    if (data.queue.thisItem.recordId) {
-      let queueRecordId = data.queue.thisItem.recordId;
-      document.getElementById("queue-record-id").setAttribute("value",
-        queueRecordId);
-    }
-	} else if (params.housingId) {
-		let campaignPath = (
-			`/contrib/affordable-housing/campaigns/${params.campaign}`);
-	  setInvalidHousingIdMessage(params.housingId, campaignPath)
-	} else {
-	  setEmptyQueueMessage(data.queue);
+	initUserName();
+	// A campaign is required for nearly all aspects of the page.
+	if (params.campaign) {
+		let safeCampaign = encodeURIComponent(params.campaign);
+	  document.getElementById("housing-changes").setAttribute("action",
+	   `/contrib/affordable-housing/thank-you?campaign=${safeCampaign}`);
+	  if (data.housing) {
+	    if (data.queue.thisItem.recordId) {
+	      let queueRecordId = data.queue.thisItem.recordId;
+	      document.getElementById("queue-record-id").setAttribute("value",
+	        queueRecordId);
+	    }
+		} else if (params.housingId) {
+			let campaignPath = (
+				`/contrib/affordable-housing/campaigns/${params.campaign}`);
+		  setInvalidHousingIdMessage(params.housingId, campaignPath)
+		} else {
+		  setEmptyQueueMessage(data.queue);
+		}
 	}
 }
 
@@ -106,12 +167,12 @@ function initPage(data, params) {
 async function run() {
 	addListeners();
 	let params = getPathParams();
+	let data = await fetchFormPrefillData(params);
+	initPage(data, params);
 	if (params.campaign) {
 	  document.getElementById("campaign").setAttribute("value", params.campaign);
-	  let safeCampaign = encodeURIComponent(params.campaign);
-	  document.getElementById("housing-changes").setAttribute("action",
-	   `/contrib/affordable-housing/thank-you?campaign=${safeCampaign}`);
-		let data = await fetchFormPrefillData(params);
-		initPage(data, params);
 	}
 }
+
+
+
