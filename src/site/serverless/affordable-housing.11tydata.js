1921a01a9e9e0787b86249161f956952808440af
trevorshannon <trevor.j.shannon@gmail.com>
2022-08-15 13:45:23 -0700

    Adds MVgo shuttle stops to map.

--- src/site/serverless/affordable-housing.11tydata.js
+++ src/site/serverless/affordable-housing.11tydata.js
@@ -1,7 +1,15 @@
 const EleventyFetch = require("@11ty/eleventy-fetch");
 
-module.exports = async function() {
-  const endpoint = `https://api.511.org/transit/stops?api_key=${process.env.SF_BAY_511_API_KEY}&operator_id=SC&format=json`;
+const OPERATOR_ID_MAP = {
+  "SC": "VTA",
+  "MV": "MVgo",
+};
+
+const fetchTransitData = async(operator_id) => {
+  if (!OPERATOR_ID_MAP.hasOwnProperty(operator_id)) {
+    return [];
+  }
+  const endpoint = `https://api.511.org/transit/stops?api_key=${process.env.SF_BAY_511_API_KEY}&operator_id=${operator_id}&format=json`;
   
   // For some reason the response includes a zero width space at
   // the beginning, so the built-in eleventy-fetch json parsing
@@ -14,10 +22,17 @@ module.exports = async function() {
   }
   let response = await EleventyFetch(endpoint, options);
   let data = JSON.parse(response.trim());
-  let transit = data.Contents.dataObjects.ScheduledStopPoint.map(x => ({
+  return data.Contents.dataObjects.ScheduledStopPoint.map(x => ({
     name: x.Name,
     lat: x.Location.Latitude,
     lng: x.Location.Longitude,
+    operator: OPERATOR_ID_MAP[operator_id],
   }));
-  return {transitData: transit};
+}
+
+module.exports = async function() {
+  let vta_transit = await fetchTransitData("SC");
+  let mvgo_transit = await fetchTransitData("MV");
+  
+  return {transitData: [].concat(vta_transit, mvgo_transit)};
 }
