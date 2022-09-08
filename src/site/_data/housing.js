b32ac2d258320c267dcaf4843bba815ae3536e22
trevorshannon <trevor.j.shannon@gmail.com>
2022-09-08 15:22:04 -0700

    Computes filter values from housing data.
    Rather than executing a separate Airtable fetch just for filter values.

--- src/site/_data/housing.js
+++ src/site/_data/housing.js
@@ -20,94 +20,6 @@ function FilterCheckbox(name, label, selected) {
   this.selected = selected || false;
 }
 
-// Gets values from all housing units that are relevant to future filtering of 
-// results.
-const fetchFilterOptions = async() => {
-  let options = [];
-  const table = base(UNITS_TABLE);
-
-  return table.select({
-      view: "API all units",
-    })
-    .all()
-    .then(records => {
-      records.forEach(function(record) {
-        // TODO(trevorshannon): Figure out how to deal with empty data properly.
-        let cityStr = "";
-        if (record.get("_CITY") !== undefined) {
-          cityStr = record.get("_CITY")[0];
-        }
-        options.push({
-          city: cityStr,
-          openStatus: record.get("STATUS"),
-          unitType: record.get("TYPE"),
-          populationsServed: record.get("_POPULATIONS_SERVED"),
-        })
-      });
-      return options;
-    });
-};
-
-const filterOptions = async() => {
-  console.log("Fetching filter options.");
-  let filterOptions = await fetchFilterOptions();
-  let cities = [...new Set(filterOptions.map(o => o.city))];
-  cities = cities.filter(x => x);
-  let openStatuses = [...new Set(filterOptions.map(o => o.openStatus))];
-  openStatuses = openStatuses.filter(x => x);
-  let unitTypes = [...new Set(filterOptions.map(o => o.unitType))];
-  unitTypes = unitTypes.filter(x => x);
-  let allPopulationsServed = filterOptions.map(o => o.populationsServed);
-  allPopulationsServed = [...new Set(allPopulationsServed.flat())];
-  allPopulationsServed = allPopulationsServed.filter(x => x);
-
-  let filterVals = [];
-  filterVals.push(new FilterSection("City", "city",
-    cities.map(x => new FilterCheckbox(x))));
-
-  // Special handling for some unit types
-  // Any "{N} Bedroom" entries that have HIGH_CAPACITY_UNIT or greater bedrooms
-  // will get grouped  together into one filter checkbox.
-  const unitTypeOptions = [];
-  const bedroomSizes = [];
-  let bedroomStr = "";
-  for (const unitType of unitTypes) {
-    const match = unitType.match(/^(\d) ?(bedroom|br)$/i);
-    if (match) {
-      bedroomSizes.push({num: parseInt(match[1]), str: unitType});
-      bedroomStr = match[2];
-    }
-  }
-  const catchallSize = Math.min(Math.max(...bedroomSizes.map(x => x.num)),
-    HIGH_CAPACITY_UNIT);
-  // Only do grouping if the unit types list includes units with at least
-  // HIGH_CAPACITY_UNIT bedrooms.
-  if (catchallSize >= HIGH_CAPACITY_UNIT) {
-    // Get all unit types that will be grouped together
-    const groupedSizes = bedroomSizes.filter(x => x.num >= catchallSize);
-    for (const bedroomSize of groupedSizes) {
-      const idx = unitTypes.indexOf(bedroomSize.str);
-      // Remove it from the unit types list so it can be grouped instead.
-      unitTypes.splice(idx, 1);
-    }
-    // Make a single entry out of all the grouped sizes.
-    const groupedStr = groupedSizes.map(x => x.str).join(", ")
-    unitTypeOptions.push(new FilterCheckbox(groupedStr, `${HIGH_CAPACITY_UNIT}+ ${bedroomStr}`));
-  }
-  unitTypeOptions.push(...unitTypes.map(x => new FilterCheckbox(x)));
-  filterVals.push(new FilterSection("Type of Unit", "unitType",
-    unitTypeOptions));
-  
-  filterVals.push(new FilterSection("Availability", "availability",
-      openStatuses.map(x => new FilterCheckbox(x))));
-  filterVals.push(new FilterSection("Populations Served", "populationsServed",
-      allPopulationsServed.map(x => new FilterCheckbox(x))));
-
-  console.log("Got filter options.");
-  return filterVals;
-}
-
-
 const fetchApartmentRecords = async() => {
   let apartments = [];
   const table = base(HOUSING_DATABASE_TABLE);
@@ -237,6 +149,60 @@ const housingData = async() => {
   return apartments;
 }
 
+const filterOptions = (housing) => {
+  //const housingCopy = JSON.parse(JSON.stringify(housing));
+  const cities = [...new Set(housing.map(h => h.city).filter(c => c))];
+  const openStatuses = [...new Set(
+    housing.map(h => h.units.map(u => u.openStatus)).flat().filter(s => s))];
+  const unitTypes = [...new Set(
+    housing.map(h => h.units.map(u => u.type)).flat().filter(t => t))];
+  const allPopulationsServed = [...new Set(
+    housing.map(h => h.populationsServed).flat().filter(p => p))];
+
+  let filterVals = [];
+  filterVals.push(new FilterSection("City", "city",
+    cities.map(x => new FilterCheckbox(x))));
+
+  // Special handling for some unit types
+  // Any "{N} Bedroom" entries that have HIGH_CAPACITY_UNIT or greater bedrooms
+  // will get grouped  together into one filter checkbox.
+  const unitTypeOptions = [];
+  const bedroomSizes = [];
+  let bedroomStr = "";
+  for (const unitType of unitTypes) {
+    const match = unitType.match(/^(\d) ?(bedroom|br)$/i);
+    if (match) {
+      bedroomSizes.push({num: parseInt(match[1]), str: unitType});
+      bedroomStr = match[2];
+    }
+  }
+  const catchallSize = Math.min(Math.max(...bedroomSizes.map(x => x.num)),
+    HIGH_CAPACITY_UNIT);
+  // Only do grouping if the unit types list includes units with at least
+  // HIGH_CAPACITY_UNIT bedrooms.
+  if (catchallSize >= HIGH_CAPACITY_UNIT) {
+    // Get all unit types that will be grouped together
+    const groupedSizes = bedroomSizes.filter(x => x.num >= catchallSize);
+    for (const bedroomSize of groupedSizes) {
+      const idx = unitTypes.indexOf(bedroomSize.str);
+      // Remove it from the unit types list so it can be grouped instead.
+      unitTypes.splice(idx, 1);
+    }
+    // Make a single entry out of all the grouped sizes.
+    const groupedStr = groupedSizes.map(x => x.str).join(", ")
+    unitTypeOptions.push(new FilterCheckbox(groupedStr, `${HIGH_CAPACITY_UNIT}+ ${bedroomStr}`));
+  }
+  unitTypeOptions.push(...unitTypes.map(x => new FilterCheckbox(x)));
+  filterVals.push(new FilterSection("Type of Unit", "unitType",
+    unitTypeOptions));
+  
+  filterVals.push(new FilterSection("Availability", "availability",
+      openStatuses.map(x => new FilterCheckbox(x))));
+  filterVals.push(new FilterSection("Populations Served", "populationsServed",
+      allPopulationsServed.map(x => new FilterCheckbox(x))));
+
+  return filterVals;
+}
 
 // Returns an object containing a list of FilterSections with each FilterSection
 // having a unique list of FilterCheckboxes encompassing all the values
@@ -257,8 +223,8 @@ module.exports = async function() {
     return data;
   }
 
-  const [filterVals, housing] = await Promise.all(
-    [filterOptions(), housingData()]);
+  const housing = await housingData();
+  const filterVals = filterOptions(housing);
   
   const data = {filterValues: filterVals, housingList: housing};
 
