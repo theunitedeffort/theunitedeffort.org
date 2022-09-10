176eb0bc29b6ce05c18276d643cce8438f2eb54f
trevorshannon <trevor.j.shannon@gmail.com>
2022-09-10 15:03:52 -0700

    Pre-renders housing list item headings.
    Cleans up whitespace

--- src/site/static/stash-prerender.11ty.js
+++ src/site/static/stash-prerender.11ty.js
@@ -10,7 +10,10 @@ exports.data = function() {
 exports.render = function(data) {
   const entries = {};
   for(const entry of data.collections.prerender) {
-    entries[entry.data.apartment.id] = entry.templateContent;
+    const snippets = entry.templateContent.split("${BADGES}");
+    entries[entry.data.apartment.id] = {
+      "heading": snippets[0], "contacts": snippets[1]
+    };
   }
 
   return JSON.stringify({housingListItems: entries});
