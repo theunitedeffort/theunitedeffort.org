cd11a10af05b53ca32428567eeb7defc6dafa0a2
trevorshannon <trevor.j.shannon@gmail.com>
2023-03-15 22:41:39 -0700

    Returns JSON

--- netlify/functions/get-client-ip.js
+++ netlify/functions/get-client-ip.js
@@ -3,6 +3,7 @@ exports.handler = function(event, context, callback) {
   console.log(`{\ncontext: ${JSON.stringify(context,null,2)},\nevent: ${JSON.stringify(event,null,2)}\n}`)
   callback(null, {
   statusCode: 200,
-  body: `Your IP address ${event.headers['x-nf-client-connection-ip']}`
+  // TODO: Remove event.headers['client-ip'] (dev only)
+  body: JSON.stringify({'client-ip': event.headers['x-nf-client-connection-ip'] || event.headers['client-ip']})
   });
 }
