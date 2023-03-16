4fb318a70af1c1f8efb4c1fa5ecf303035da1a3d
trevorshannon <trevor.j.shannon@gmail.com>
2023-03-15 22:17:25 -0700

    Updates IP header name.

--- netlify/functions/get-client-ip.js
+++ netlify/functions/get-client-ip.js
@@ -3,6 +3,6 @@ exports.handler = function(event, context, callback) {
   console.log(`{\ncontext: ${JSON.stringify(context,null,2)},\nevent: ${JSON.stringify(event,null,2)}\n}`)
   callback(null, {
   statusCode: 200,
-  body: `Your IP address ${event.headers['client-ip']}`
+  body: `Your IP address ${event.headers['x-nf-client-connection-ip']}`
   });
 }
