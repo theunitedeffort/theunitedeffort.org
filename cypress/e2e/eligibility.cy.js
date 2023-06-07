6372d7ff790d826bfcbb37c9033ff3c193640b63
trevorshannon <trevor.j.shannon@gmail.com>
2023-06-06 21:23:49 -0700

    Enable linting for cypress dir.

--- cypress/e2e/eligibility.cy.js
+++ cypress/e2e/eligibility.cy.js
@@ -5,11 +5,10 @@ describe('Eligibility Assessment Tool', () => {
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
-    cy.visit('/public-assistance/eligibility/')
+    cy.visit('/public-assistance/eligibility/');
   });
 
   it('goes to the next page', () => {
     cy.contains('button', 'Next').click();
   });
-
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,15 +1,15 @@
 // https://dev.to/tylerben/catching-console-error-statements-in-cypress-1b4g
-Cypress.on("window:before:load", win => {
-  cy.stub(win.console, "error").callsFake(msg => {
+Cypress.on('window:before:load', (win) => {
+  cy.stub(win.console, 'error').callsFake((msg) => {
     // Log to the terminal
-    cy.now("task", "error", msg)
+    cy.now('task', 'error', msg);
     // Log to Command Log and fail the test
-    throw new Error(msg)
+    throw new Error(msg);
   });
 
-  cy.stub(win.console, "warn").callsFake(msg => {
+  cy.stub(win.console, 'warn').callsFake((msg) => {
     // Log to the terminal
-    cy.now("task", "warn", msg)
+    cy.now('task', 'warn', msg);
   });
 });
 
