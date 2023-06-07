d95ab2d25d8b69ee8dd69e3ad5f229b26b35bf82
trevorshannon <trevor.j.shannon@gmail.com>
2023-06-06 21:15:31 -0700

    Adds more form interactions.

--- cypress/e2e/eligibility.cy.js
+++ cypress/e2e/eligibility.cy.js
@@ -1,31 +1,39 @@
 
 describe('Eligibility Assessment Tool', () => {
   beforeEach(() => {
-    // Cypress starts out with a blank slate for each test
-    // so we must tell it to visit our website with the `cy.visit()` command.
-    // Since we want to visit the same URL at the start of all our tests,
-    // we include it in our beforeEach function so that it runs before each test
     cy.visit('/public-assistance/eligibility/')
   });
 
-  it('gets to final results page with no data entry', () => {
+  it('gets to final results page with data entry', () => {
     cy.get('#page-intro').should('be.visible');
     cy.get('#next-button').click();
+
     cy.get('#page-yourself-start').should('be.visible');
+    cy.get('#age').type('42');
+    cy.get('#not-citizen').click();
     cy.get('#next-button').click();
+
+    cy.get('#page-immigration-status').should('be.visible');
+    cy.get('#next-button').click();
+
     cy.get('#page-household-members').should('be.visible');
     cy.get('#next-button').click();
+
     cy.get('#page-household-situation').should('be.visible');
     cy.get('#next-button').click();
+
     cy.get('#page-income').should('be.visible');
     cy.get('#next-button').click();
+
     cy.get('#page-ss-taxes').should('be.visible');
     cy.get('#next-button').click();
+
     cy.get('#page-income-assets').should('be.visible');
     cy.get('#next-button').click();
+
     cy.get('#page-existing-benefits').should('be.visible');
     cy.get('#submit-button').click();
+
     cy.get('#page-results').should('be.visible');
   });
-
 });
