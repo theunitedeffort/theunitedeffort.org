573ab9fdb06f6c57e82f5114a9fd5fbe8764984e
DairyProducts <66636627+DairyProducts@users.noreply.github.com>
2023-06-08 20:10:06 -0700

    Added tests and additional instructions on UI

--- src/site/_includes/js/eligibility.js
+++ src/site/_includes/js/eligibility.js
@@ -545,10 +545,11 @@ function onHouseholdMemberAdd(event) {
   // Add listener to the new member's spouse checkbox
   const spouseInput = newMember.querySelector('[id^="hh-member-spouse"]');
   spouseInput.addEventListener('click', onChangeSpouse);
-  spouseInput.dependentParameter = newMember.querySelector('[id^="hh-member-dependent"]');
+  // Add listener to the new member's dependent checkbox
   const dependentInput = newMember.querySelector('[id^="hh-member-dependent"]');
   dependentInput.addEventListener('click', onChangeDependent);
-  dependentInput.spouseParameter = newMember.querySelector('[id^="hh-member-spouse"]');
+  spouseInput.dependentParameter = dependentInput;
+  dependentInput.spouseParameter = spouseInput;
   // Add listener to the name input so the heading can be updated.
   const nameInput = newMember.querySelector('[id^="hh-member-name"]');
   nameInput.addEventListener('change', onChangeName);
