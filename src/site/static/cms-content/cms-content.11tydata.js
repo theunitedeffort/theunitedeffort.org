d93562fc54458e96f7af92c068b429be7fff8c64
trevorshannon <trevor.j.shannon@gmail.com>
2023-09-22 20:21:15 -0700

    Refactors stories into pages code.

--- src/site/static/cms-content/cms-content.11tydata.js
+++ src/site/static/cms-content/cms-content.11tydata.js
@@ -1,4 +1,5 @@
 const {AssetCache} = require('@11ty/eleventy-fetch');
+const Image = require("@11ty/eleventy-img");
 const Airtable = require('airtable');
 const base = new Airtable(
   {apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_ID);
@@ -79,17 +80,80 @@ const fetchGeneralResources = async () => {
     });
 };
 
+// Fetch the list of stories from the Airtable API.
+const fetchStories = async () => {
+  const data = [];
+  const table = base('tblD0j9sZoGc41MSQ'); // Client stories table
+  return table.select({
+    view: 'API list all',
+  })
+    .all()
+    .then((records) => {
+      records.forEach(function(record) {
+        if (record.get('Status') == 'Published') {
+          data.push(record.fields);
+        }
+      });
+      return data;
+    });
+};
+
+const fetchImages = async (stories) => {
+  for (const story of stories) {
+    if (story['Photo'] && story['Photo'].length > 0) {
+      const stats = await Image(story['Photo'][0].url, {
+        widths: [500, 200],
+        urlPath: "/images/",
+        outputDir: "./dist/images/",
+      });
+      story.image = stats.jpeg[1];
+      story.thumb = stats.jpeg[0];
+    }
+  }
+}
+
+
+// module.exports = async function() {
+//   const asset = new AssetCache('airtable_stories');
+//   if (asset.isCacheValid('1h')) {
+//     console.log('Returning cached stories data.');
+//     return await asset.getCachedValue();
+//   }
+//   console.log('Fetching stories.');
+//   const storyList = await fetchStories();
+//   for (const story of storyList) {
+//     if (story['Photo'] && story['Photo'].length > 0) {
+//       const stats = await Image(story['Photo'][0].url, {
+//         widths: [500, 200],
+//         urlPath: "/images/",
+//         outputDir: "./dist/images/",
+//       });
+//       story.image = stats.jpeg[1];
+//       story.thumb = stats.jpeg[0];
+//     }
+//   }
+//   const ret = {stories: storyList};
+//   await asset.save(ret, 'json');
+//   return ret;
+
 
 module.exports = async function() {
   const asset = new AssetCache('airtable_pages');
-  if (asset.isCacheValid('1h')) {
+  if (asset.isCacheValid('1s')) {
     console.log('Returning cached pages data.');
     return await asset.getCachedValue();
   }
   console.log('Fetching pages.');
-  const [pageList, resourceList] = await Promise.all(
-    [fetchPages(), fetchGeneralResources()]);
-  const ret = {pages: pageList, partialsData: {resources: resourceList}};
+  const [pageList, resourceList, storiesList] = await Promise.all(
+    [fetchPages(), fetchGeneralResources(), fetchStories()]);
+  await fetchImages(storiesList);
+  const ret = {
+    pages: pageList,
+    partialsData: {
+      resources: resourceList,
+      stories: storiesList,
+    },
+  };
   await asset.save(ret, 'json');
   return ret;
 };
