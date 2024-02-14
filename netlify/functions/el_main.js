ca9593ceb7826cc1743baf6e924ab3154e962dc4
trevorshannon <trevor.j.shannon@gmail.com>
2024-02-13 18:28:59 -0800

    Fully removes gen204 src attribute.

--- netlify/functions/el_main.js
+++ netlify/functions/el_main.js
@@ -13,7 +13,7 @@ exports.handler = async function(event) {
   const resp = await fetch(endpoint);
   let content = await resp.text();
   content = content.replaceAll('https://www.google.com/images/cleardot.gif', 'https://');
-  content = content.replaceAll('/gen204?', '/');
+  content = content.replace(/src=.*?\/gen204\?.*?;/g, 'src="";');
 
   return {
     statusCode: resp.status,
