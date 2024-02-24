2c23da9f085f1674f832fbedbb0fc77bc722418a
trevorshannon <trevor.j.shannon@gmail.com>
2024-02-23 21:18:22 -0800

    Adds basic translation e2e test.

---
+++ cypress/e2e/translation.cy.js
@@ -0,0 +1,23 @@
+describe('Client-side Translation', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+  it('defaults to English', () => {
+    cy.findByText('Our Mission').should('exist');
+  });
+
+  it('translates to Spanish', () => {
+    cy.findByRole('link', {name: 'Español'}).click();
+    cy.findByText('Nuestra misión').should('exist');
+  });
+
+  it('translates to Vietnamese', () => {
+    cy.findByRole('link', {name: 'Tiếng Việt'}).click();
+    cy.findByText('Nhiệm vụ của chúng ta').should('exist');
+  });
+
+  it('translates to Chinese', () => {
+    cy.findByRole('link', {name: '简体中文'}).click();
+    cy.findByText('我们的任务').should('exist');
+  });
+});
