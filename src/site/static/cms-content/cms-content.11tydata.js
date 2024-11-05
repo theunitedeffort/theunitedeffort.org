3167a8ba1c81bb3946d8bd25599dbc729b0fc9ee
trevorshannon <trevor.j.shannon@gmail.com>
2024-11-05 14:15:58 -0800

    Replaces missing line.

--- src/site/static/cms-content/cms-content.11tydata.js
+++ src/site/static/cms-content/cms-content.11tydata.js
@@ -166,6 +166,7 @@ const fetchAssets = async () => {
 
 module.exports = async function() {
   const asset = new eleventyFetch.AssetCache('airtable_pages');
+  if (asset.isCacheValid('1h')) {
     console.log('Returning cached pages data.');
     return await asset.getCachedValue();
   }
