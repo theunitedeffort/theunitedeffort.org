285b91913bc6c4b19d970eeb1d2f4908b98cf842
trevorshannon <trevor.j.shannon@gmail.com>
2024-11-06 12:00:34 -0800

    Selects external campaign page for donations.

--- src/site/static/cms-content/cms-content.11tydata.js
+++ src/site/static/cms-content/cms-content.11tydata.js
@@ -31,7 +31,7 @@ const fetchPages = async () => {
     .all()
     .then(async (records) => {
       for (const record of records) {
-        if (true) {
+        if (record.get('Status') == 'Published') {
           const name = record.get('Page title');
           const path = record.get('Page path');
           const sectionID = record.get('Section')[0];
