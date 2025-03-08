a45bf343dd25e84c2dce83c3e9f859f6b103595f
DairyProducts <66636627+DairyProducts@users.noreply.github.com>
2025-03-07 17:03:06 -0800

    Add Complex Discharge Flag

--- src/site/_includes/js/eligibility.js
+++ src/site/_includes/js/eligibility.js
@@ -1589,6 +1589,15 @@ function complexImmigration(input,
     complexOptions.includes(input.immigrationStatus));
 }
 
+// Returns true if discharge status is other-than-honorable, bad conduct,
+// or dishonorable. Used to determine complex discharge flag.
+function complexDischarge(input,
+  complexOptions=['dishonorable', 'oth', 'bad-conduct',]) {
+    return (
+    complexOptions.includes(input.dischargeStatus));
+  
+}
+
 // Computes the incremental change in HUD income limit for large family sizes.
 // 'numExtraPeople' is the number of people beyond the max family size given
 // by the array of income limits given by 'limits'.  For example, if 'limits'
@@ -1654,6 +1663,7 @@ const FlagCodes = {
   NEAR_INCOME_LIMIT: 'NEAR_INCOME_LIMIT',
   TOO_COMPLEX: 'TOO_COMPLEX',
   COMPLEX_IMMIGRATION: 'COMPLEX_IMMIGRATION',
+  COMPLEX_DISCHARGE: 'COMPLEX_DISCHARGE',
   COMPLEX_RETIREMENT_AGE: 'COMPLEX_RETIREMENT_AGE',
   MORE_INFO_NEEDED: 'MORE_INFO_NEEDED',
 };
@@ -2115,6 +2125,9 @@ function vaDisabilityResult(input) {
 
   if (input.existingVaDisabilityMe) {
     program.markEnrolled();
+  }
+  if (complexDischarge(input)) {
+    program.addFlag(FlagCodes.COMPLEX_DISCHARGE);
   }
   return program.getResult();
 }
@@ -2651,6 +2664,9 @@ function vaPensionResult(input) {
   if (input.existingVaPensionMe) {
     program.markEnrolled();
   }
+  if (complexDischarge(input)) {
+    program.addFlag(FlagCodes.COMPLEX_DISCHARGE);
+  }
   return program.getResult();
 }
 
@@ -3020,6 +3036,13 @@ function renderFlags(flags, listElem) {
     case FlagCodes.COMPLEX_IMMIGRATION:
       flagMsg = 'The immigrant eligibility rules for this program are ' +
         'complex, and not all immigrants are eligible.';
+      break;
+    case FlagCodes.COMPLEX_DISCHARGE:
+      flagMsg = 'Veterans receiving undesirable, bad conduct, and other ' +
+        'than honorable discharges may qualify for VA benefits depending ' + 
+        'on a determination made by VA. ' +
+        '<a href="https://www.benefits.va.gov/benefits/character_of_discharge.asp" ' +
+        'target="_blank" rel="noopener">Learn More</a>';
       break;
     case FlagCodes.COMPLEX_RETIREMENT_AGE:
       flagMsg = 'To be eligible for this program, you must be younger than ' +
